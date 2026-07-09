2d5ba066158f48eacf02bd0b650d7af599c05b8d
El Hadji Malick Seye <eljimalickseye@gmail.com>
2026-07-09 23:50:20 +0000

    Update API base URL to VPS reverse proxy endpoint

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -4,7 +4,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class ApiService {
   // Configuration pour le VPS de production. Modifiez cette URL lors de votre déploiement.
-  static const String baseUrl = 'http://180.149.199.233:8000/api'; 
+  static const String baseUrl = 'http://180.149.199.233/teptep-api/api'; 
   static String? token;
 
   static Future<void> init() async {
